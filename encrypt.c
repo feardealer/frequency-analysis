73445af84f36e0fe5235b20d7311c6dd0a987e2f
Vadim <fancybear@internet.ru>
2023-04-14 09:42:54 +0400

    update encryption methods

--- encrypt.c
+++ encrypt.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 int main(int argc, char *argv[])
 {
@@ -14,15 +16,47 @@ int main(int argc, char *argv[])
                 }
 		
 		fptr2 = fopen("encrypted.txt", "w+");
+		
+    printf("[1] - ASCII code\n");
+		printf("[2] - Caesar\n");
+		printf("[3] - Vigenere\n");
+    printf("Choice: ");
+		
+		int choice;
+		scanf("%d", &choice);
 
-		while ((c = fgetc(fptr1)) != EOF) {
-			fprintf(fptr2, "%d ", c);
+    if (choice == 1) {
+			while ((c = fgetc(fptr1)) != EOF) {		
+        if (isalpha(c))
+          fprintf(fptr2, "%d ", c);
+        else
+          fprintf(fptr2, "%c", c);
+      }
+    }
+		if (choice == 2) {
+			int key;
+			printf("%s", "Key: ");
+			scanf("%d", &key);
+			
+			while ((c = fgetc(fptr1)) != EOF) {		
+				if (isalpha(c))
+					fprintf(fptr2, "%c", (tolower(c) - 'a' + key) % 26 + 'a');
+				else
+					fprintf(fptr2, "%c", c);
+			}
+		}
+    if (choice == 3) {
+      
+    }
+		if (choice != 1 && choice != 2 && choice != 3) {
+			printf("Wrong cipher\n");
+			return 1;
 		}
 
 		fclose(fptr1);
 		fclose(fptr2);
 
-		printf("Done!");
+		printf("Done!\n");
 
 	} else {
 		printf("Error.");
