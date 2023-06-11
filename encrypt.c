8e313258d244b65abd25cb9bc8d8f164ffb35d79
Vadim <fancybear@internet.ru>
2023-06-11 22:24:24 +0400

    add vigenere cipher

--- encrypt.c
+++ encrypt.c
@@ -1,66 +1,61 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <stdio.h>
 #include <ctype.h>
 
-int main(int argc, char *argv[])
-{
-	if (argc == 2) {
-		FILE *fptr1 = NULL, *fptr2 = NULL;
-		char c;
-		fptr1 = fopen(argv[1], "r");
+int main(int argc, char * argv[]) {
+  if (argc == 2) {
+    FILE * fptr1 = NULL, * fptr2 = NULL;
+    fptr1 = fopen(argv[1], "r");
 
-		if (fptr1 == NULL) {
-                	printf("%s: cannot access '%s': No such file or directory", argv[0], argv[1]);
-                        return 1;
-                }
-		
-		fptr2 = fopen("encrypted.txt", "w+");
-		
-    printf("[1] - ASCII code\n");
-		printf("[2] - Caesar\n");
-		printf("[3] - Vigenere\n");
-    printf("Choice: ");
-		
-		int choice;
-		scanf("%d", &choice);
+    if (fptr1 == NULL) {
+      printf("%s: cannot access '%s': No such file or directory", argv[0], argv[1]);
+      return 1;
+    }
 
-    if (choice == 1) {
-			while ((c = fgetc(fptr1)) != EOF) {		
-        if (isalpha(c))
-          fprintf(fptr2, "%d ", c);
-        else
-          fprintf(fptr2, "%c", c);
+    fptr2 = fopen("encrypted.txt", "w+");
+    printf("%s", "...\n");
+
+    char c, * message = malloc(0), * keyword = malloc(0);
+    int i = 0, key, typechar, keysize = 0, messagesize = 0;
+
+    printf("Key: ");
+    while ((c = getchar()) != '\n') {
+      if (c >= 65 && c <= 90 || c >= 97 && c <= 122) {
+        keyword = realloc(keyword, (keysize + 1) * sizeof(char));
+        keyword[keysize] = c;
+        keysize++;
       }
+
     }
-		if (choice == 2) {
-			int key;
-			printf("%s", "Key: ");
-			scanf("%d", &key);
-			
-			while ((c = fgetc(fptr1)) != EOF) {		
-				if (isalpha(c))
-					fprintf(fptr2, "%c", (tolower(c) - 'a' + key) % 26 + 'a');
-				else
-					fprintf(fptr2, "%c", c);
-			}
-		}
-    if (choice == 3) {
-      
+
+    while ((c = fgetc(fptr1)) != EOF) {
+      message = realloc(message, (messagesize + 1) * sizeof(char));
+      message[messagesize] = c;
+      messagesize++;
     }
-		if (choice != 1 && choice != 2 && choice != 3) {
-			printf("Wrong cipher\n");
-			return 1;
-		}
 
-		fclose(fptr1);
-		fclose(fptr2);
+    for (int j = 0; j < messagesize; j++) {
+      c = message[j];
 
-		printf("Done!\n");
+      if (isalpha(c)) {
+        typechar = (c < 95) ? 65 : 97;
+        c -= typechar;
+        key = (int) keyword[i];
+        typechar = (key < 95) ? 65 : 97;
+        key -= typechar;
+        c = (c + key) % 26;
+        fprintf(fptr2, "%c", c + 65);
+        i = (i + 1) % keysize;
+      } else fprintf(fptr2, "%c", c);
+    }
 
-	} else {
-		printf("Error.");
-		return 1;
-	}
-	return 0;
+    fclose(fptr1);
+    fclose(fptr2);
+
+    printf("Done!\n");
+  } else {
+    printf("Error. Usage: ./encrypt <SOURCE TEXT FILE>\n");
+    return 1;
+  }
+  return 0;
 }
